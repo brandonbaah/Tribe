b6405dc769e2e00b54a178c7c173135656588a8f
Brandon Baah <brandonbaah@gmail.com>
2020-08-13 09:51:16 -0400

    chore: get all posts
    Added method to Posts Model
    Use DB facade

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Post as Post;
 use App\Time as Time;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
@@ -15,7 +16,9 @@ class PostController extends Controller
      */
     public function index()
     {
-        dd('Post');
+        $allPost = new Post;
+        dd($allPost->getPosts());
+
     }
 
     /**
--- app/Post.php
+++ app/Post.php
@@ -4,11 +4,23 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\DB;
 
 class Post extends Model
 {
     use SoftDeletes;
     protected $guarded = [];
 
-
+    public function getPosts()
+    {
+        return DB::table('posts')->select(
+            'id',
+            'start_time',
+            'end_time',
+            'in_home',
+            'date',
+            'notes',
+            'created_at'
+        )->get();
+    }
 }
